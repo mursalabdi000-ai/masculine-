c99c7797772943b721107877ba3a06851a7ce99f
Masculine Security <mursal.abdi000@gmail.com>
2026-02-04 18:38:48 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,23 @@
-const CACHE = "ms-office-v1";
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ])
-    )
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+{
+  "id": "/masculine/",
+  "name": "Masculine Security – Mini Office",
+  "short_name": "MS Office",
+  "start_url": "/masculine/",
+  "scope": "/masculine/",
+  "display": "standalone",
+  "background_color": "#0b1220",
+  "theme_color": "#0b1220",
+  "description": "Mini Office App – Rechnungen, Kunden, Ausgaben, Lohnabrechnung, Einkommensteuer",
+  "icons": [
+    {
+      "src": "/masculine/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/masculine/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
