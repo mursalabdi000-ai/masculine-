1c4970c6a590acc10bee793051c90687a37af798
Masculine Security <mursal.abdi000@gmail.com>
2026-02-05 17:11:21 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,39 @@
-const CACHE = "ms-cache-v1";
+const CACHE_NAME = "ms-mini-office-v2";
 
-const FILES = [
-  "/masculine/",
-  "/masculine/index.html",
-  "/masculine/style.css",
-  "/masculine/manifest.json"
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+// Activate + clear old cache
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
