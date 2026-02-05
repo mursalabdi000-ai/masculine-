7f2e0294dee53ed21665c46597188b113b18970f
Masculine Security <mursal.abdi000@gmail.com>
2026-02-05 16:43:01 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,18 @@
-const CACHE_NAME = "ms-mini-office-v1";
+const CACHE = "ms-cache-v1";
+
 const FILES = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "/masculine/",
+  "/masculine/index.html",
+  "/masculine/style.css",
+  "/masculine/manifest.json"
 ];
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
